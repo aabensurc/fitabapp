fb6c0d6708549da2cd15677a25a28be50d862a0f
aabensurc <31977653+aabensurc@users.noreply.github.com>
2024-10-09 12:39:26 -0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,23 @@
+// Leer el archivo JSON y cargar los datos
+// fetch('json/data.json')
+//   .then(response => response.json())
+//   .then(data => {
+//     let content = document.getElementById('content');
+//     let html = '';
+
+//     data.forEach(item => {
+//       html += `
+//         <div class="col-md-4">
+//           <div class="card">
+//             <div class="card-body">
+//               <h5 class="card-title">${item.title}</h5>
+//               <p class="card-text">${item.description}</p>
+//             </div>
+//           </div>
+//         </div>
+//       `;
+//     });
+
+//     content.innerHTML = html;
+//   })
+//   .catch(error => console.error('Error al cargar el JSON:', error)); 
