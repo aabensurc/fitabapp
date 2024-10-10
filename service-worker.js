8632892384b7fda84e0c0ddd9e36f4ebfeab1409
aabensurc <31977653+aabensurc@users.noreply.github.com>
2024-10-09 22:22:38 -0500

    Update service-worker.js
    edicion

--- service-worker.js
+++ service-worker.js
@@ -8,7 +8,18 @@ self.addEventListener('install', event => {
           'css/styles.css',
           'js/app.js',
           'json/data.json',
-          'manifest.json'
+          'manifest.json',
+          'sounds/timesup.mp3',
+          'fonts/Gilroy-Bold.ttf',
+          'fonts/Gilroy-Heavy.ttf',  
+          'fonts/Gilroy-Light.ttf',  
+          'fonts/Gilroy-Medium.ttf',  
+          'fonts/Gilroy-Regular.ttf',    
+          'img/logo.png', 
+          'img/icon1.png',      // Icono para el manifest
+          'img/icon2.png',      // Otro icono
+          'img/screenshot1.png', // Captura de pantalla
+          'img/screenshot2.png'  // Otra captura de pantalla
         ]);
       })
     );
