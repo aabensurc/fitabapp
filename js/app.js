19b28931f7516a1bf7af007c2f41a2949c60a3b9
aabensurc <31977653+aabensurc@users.noreply.github.com>
2024-10-11 12:18:04 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,23 +1,15 @@
-// Leer el archivo JSON y cargar los datos
-// fetch('json/data.json')
-//   .then(response => response.json())
-//   .then(data => {
-//     let content = document.getElementById('content');
-//     let html = '';
-
-//     data.forEach(item => {
-//       html += `
-//         <div class="col-md-4">
-//           <div class="card">
-//             <div class="card-body">
-//               <h5 class="card-title">${item.title}</h5>
-//               <p class="card-text">${item.description}</p>
-//             </div>
-//           </div>
-//         </div>
-//       `;
-//     });
-
-//     content.innerHTML = html;
-//   })
-//   .catch(error => console.error('Error al cargar el JSON:', error)); 
+// Evento mousedown para el efecto ripple
+document.querySelectorAll('html').forEach(item => {
+    item.addEventListener('click', function(e) {
+        let ripple = document.createElement('span');
+        ripple.classList.add('ripple-effect');
+        ripple.style.left = `${e.clientX - e.target.offsetLeft}px`;
+        ripple.style.top = `${e.clientY - e.target.offsetTop}px`;
+        this.appendChild(ripple);
+        
+        // Eliminar el ripple después de 500ms
+        setTimeout(() => {
+            ripple.remove();
+        }, 500); 
+    });
+});
