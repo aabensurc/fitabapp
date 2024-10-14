d5d643f82c9c29a6866fa6d05ae844f1cd91ee1d
aabensurc <31977653+aabensurc@users.noreply.github.com>
2024-10-14 08:23:31 -0500

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,11 +1,32 @@
 // auth.js
 
-// Función para validar si el usuario está logueado
+// Función para validar si el usuario está logueado y habilitado
 function checkLogin() {
     if (localStorage.getItem('loggedIn') !== 'true') {
-        // Limpiar el historial de la página anterior y redirigir a login.html
-        history.replaceState(null, null, 'login.html'); 
-        window.location.href = 'login.html';
+        // Si no está logueado, redirigir a login.html
+        location.replace('login.html');
+    } else {
+        // Obtener el nombre de usuario desde el localStorage
+        const username = localStorage.getItem('username');
+
+        // Hacer una petición para obtener el archivo usuarios.json
+        fetch('json/usuarios.json')
+            .then(response => response.json())
+            .then(data => {
+                // Buscar al usuario en la lista de usuarios
+                const usuario = data.find(u => u.nombre_usuario === username);
+
+                // Si no se encuentra el usuario o el estado es "Deshabilitado", redirigir a login.html
+                if (!usuario || usuario.estado === 'Deshabilitado') {
+                    alert('Tu cuenta está deshabilitada. Contacta al administrador.');
+                    logout(); // Cerrar sesión y redirigir a login.html
+                }
+            })
+            .catch(error => {
+                console.error('Error al cargar el archivo JSON:', error);
+                // Si ocurre un error en la consulta, redirigir a login.html como medida de seguridad
+                location.replace('login.html');
+            });
     }
 }
 
@@ -13,6 +34,12 @@ function checkLogin() {
 function logout() {
     localStorage.removeItem('loggedIn');
     localStorage.removeItem('username');
-    history.replaceState(null, null, 'login.html'); 
-    window.location.href = 'login.html';
+    localStorage.removeItem('nombre');
+    localStorage.removeItem('apellido');
+    localStorage.removeItem('sexo');
+    localStorage.removeItem('edad');
+    localStorage.removeItem('peso');
+    localStorage.removeItem('estatura');
+    localStorage.removeItem('estado');
+    location.replace('login.html');
 }
